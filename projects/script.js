46b3614d149cdd323029327ed793b4c452cc9db3
ashafiuddin21 <102944317+ashafiuddin21@users.noreply.github.com>
2022-04-16 15:32:59 -0500

    Simple Update

---
+++ projects/script.js
@@ -0,0 +1,15 @@
+var p1mon = 200;
+var p2mon = 200;
+console.log("Player 1 has " + p1mon + " dollars.");
+
+//player 1 
+
+
+//let p1bet = prompt("How much would you like to bet?");
+//let p2bet = prompt("How much would you like to bet?");
+
+//alert("Player 1 betted " + p1bet + " dollars.");
+// ask for input on player 1 to place a bet
+
+
+
