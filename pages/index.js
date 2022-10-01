c2204a509c7fc1ce0ff5944dc92de9c337aeb3ec
Akbaba© <52055180+AKbaba2@users.noreply.github.com>
2022-10-01 13:06:13 -0500

    Home page update
    Added a sample jumbotron, added grid function, made custom container functions.

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,32 @@ function Home() {
     <>
       <Navbar />
       <div class="home">
-        <div class="showcase">
-          <div class="left">
-            {/* Header below */}
-            <h1>Welcome to the Computer Science Club!</h1> 
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra ipsum nunc aliquet bibendum enim facilisis gravida neque. Mollis nunc sed id semper risus. Elementum tempus egestas sed sed risus. Dictum fusce ut placerat orci nulla pellentesque dignissim enim sit.</p>
-            {/* Button below */}
-            <a href="" target="_blank" class="btn">See Our Projects</a> 
+
+        {/* Jumbotron */}
+        <section class="jumbotron">
+          <div class="container">
+            <div class="jumbotron-text">
+              <h1>Computer Science Club</h1>
+              <p>The best club in the world</p>
+            </div>
           </div>
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/LZ-f2hMaJKs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>
+        </section>
+
+        {/* Showcase */}
+        <section class="showcase">
+          <div class="container">
+            <div class="showcase-box grid">
+              <div class="left">
+                {/* Header below */}
+                <h1>Welcome to the Computer Science Club!</h1> 
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra ipsum nunc aliquet bibendum enim facilisis gravida neque. Mollis nunc sed id semper risus. Elementum tempus egestas sed sed risus. Dictum fusce ut placerat orci nulla pellentesque dignissim enim sit.</p>
+                {/* Button below */}
+                <a href="" target="_blank" class="btn">See Our Projects</a> 
+              </div>
+              <iframe id="intro-vid" width="560" height="315" src="https://www.youtube.com/embed/LZ-f2hMaJKs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+          </div>
+        </section>
       </div>
     </>
   )
