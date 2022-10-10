8975f92579cdd9d1868a982f71ecdbf83f56b2eb
Akbaba© <52055180+AKbaba2@users.noreply.github.com>
2022-10-09 22:12:40 -0500

    Added info-cards (Color not final)
    Basically we can put short info in these 'cards'. The color obviously needs changing though. This is just what I came up with on the spot.

--- pages/index.js
+++ pages/index.js
@@ -24,10 +24,10 @@ function Home() {
           <div class = "showcase-box grid">
             <div class = "left">
               { /* Header below */ }
-              <h1> Welcome to the Computer Science Club! </h1>
-              <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Viverra ipsum nunc aliquet bibendum enim facilisis gravida neque.Mollis nunc sed id semper risus.Elementum tempus egestas sed sed risus.Dictum fusce ut placerat orci nulla pellentesque dignissim enim sit. </p>
+              <h1>Welcome to the Computer Science Club!</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra ipsum nunc aliquet bibendum enim facilisis gravida neque. Mollis nunc sed id semper risus. Elementum tempus egestas sed sed risus. Dictum fusce ut placerat orci nulla pellentesque dignissim enim sit.</p>
               { /* Button below */ }
-              <a href = "" class = "button">See Our Projects </a>
+              <a href = "" class = "button">See Our Projects</a>
             </div>
             <iframe id = "intro-vid" width = "560" height = "315" src = "https://www.youtube.com/embed/LZ-f2hMaJKs" title = "YouTube video player" frameborder = "0" allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </div>
@@ -37,7 +37,24 @@ function Home() {
       { /* Info cards */ }
       <div class = "info">
         <div class = "container">
-
+          <div class = "info-wrapper">
+            <div class="first">
+              <h3>First</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="second">
+              <h3>Second</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="third">
+              <h3>Third</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="fourth">
+              <h3>Fourth</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra ipsum nunc aliquet bibendum enim facilisis gravida neque. Mollis nunc sed id semper risus. Elementum tempus egestas sed sed risus. Dictum fusce ut placerat orci nulla pellentesque dignissim enim sit.</p>
+            </div>
+          </div>
         </div>
       </div>
 
