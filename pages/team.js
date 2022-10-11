a905b4ba6b6b30200248d01f1c5ff77a5fd47ffb
Voy7 <89422331+Voy7@users.noreply.github.com>
2022-10-11 14:23:08 -0500

    Meet The Team page update

--- pages/team.js
+++ pages/team.js
@@ -9,7 +9,7 @@ function Team() {
     <>
       <Navbar />
       <div id="team">
-      <section class="jumbotron">
+        <section class="jumbotron">
           <div class="container">
             <div class="jumbotron-text">
               <h1>Meet The Team</h1>
@@ -18,16 +18,30 @@ function Team() {
         </section>
 
         { /*About the team*/ }
-      <div class = "showcase">
-        <div class = "container">
-          <div class = "showcase-box grid">
-            <div class = "left">
-              <h1>About the Team</h1>
+        <div class = "showcase">
+          <div class = "container">
+            <div class = "showcase-box grid">
+              <div class = "left">
+                <h1>About the Team</h1>
+              </div>
             </div>
           </div>
         </div>
-        
-      </div>
+
+        <div id="people-container">
+          {
+            people.map(person => {
+              return (
+                <a class="person" href={'/people/' + person.name}>
+                  <img src={person.image} />
+                  <h3>{person.name}</h3>
+                  <span class={"role role-" + person.role}>{person.role}</span>
+                  <span class="info">More Info &rarr;</span>
+                </a>
+              )
+            })
+          }
+        </div>
       </div>
       
     </>
