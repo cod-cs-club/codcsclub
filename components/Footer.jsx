73c49eda69c9c997057bb5462a316b2d800c0b23
Natalia935 <nataliafigueroa589@gmail.com>
2022-11-09 18:22:28 -0600

    .

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,23 @@
 function Footer() {
   return (
     <footer id="footer">
-      <img src="/CSLogoWhite.png" alt="Website logo" />
+      <div className="col-1"></div>
+            <h3>CONTACT US</h3>
+      <div className="col-2"></div>
+            <a href="/">Home</a>
+      <div className="col-3">
+            <p>About us</p>
+            <br>
+            </br>
+            <p>Projects</p>
+            <br>
+            </br>
+            <p>Gallery</p>
+            <br>
+            </br>
+            <p>Frequently asked questions</p>
+      </div>
+            <img src="/CSLogoWhite.png" alt="Website logo" />
       <p>&copy; Website created by the College of Dupage Computer Science Club</p>
     </footer>
   )
